aa1216d3bd4fe799c8a1024ae29ae3372bbaa024
camille147 <camille.pinault@coda-student.school>
2024-09-26 16:34:46 +0200

    etape 3 : recupérer les infos

--- main.c
+++ main.c
@@ -5,5 +5,11 @@ int main()
 	int nb_1;
 	int nb_2;
 	char oper;
+	printf("Bonjour, veuillez choisir un opérateur parmi ceux_ci: +,-,*,/,%%");
+	scanf("%c", oper);
+	printf("Veuillez choisir un premier nombre");
+	scanf("%d", nb_1)
+	printf("Veuillez choisir un deuxième nombre");
+	scanf("%d", nb_2);
 exit(0)
 }
